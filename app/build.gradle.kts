3932aa8b2910dbf08b7fcdd3c9f3614881efbf28
ekhiw <ekkyhiw@gmail.com>
2025-06-12 00:46:59 +0700

    Add Filter Enum
    - Add live data (total task, complete task, and filtered task)
    - Add filter to repository and dao
    - Observe live data as state with default value

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.material3)
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.runtime.livedata)
     annotationProcessor(libs.androidx.room.compiler)
     ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
